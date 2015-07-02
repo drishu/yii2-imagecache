00a05cf5c4d7f4dad3be3c3f7c1993d17bbf3c33
Andras Szilagyi <szilagyi_an@yahoo.com>
2015-07-02 14:27:43 +0300

    Adding the main class file

---
+++ ImageCache.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace drishu\yii2imagecache;
+
+use Yii;
+
+/**
+ * @author Szilagyi Andras <mr.drishu@gmail.com>
+ */
+class ImageCache extends \yii\base\Component
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
